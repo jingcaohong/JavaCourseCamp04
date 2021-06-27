301cda7b79bbcd73c8bcc53031b6a412dcfbacd8
jingcaohong <jingcaohong@gmail.com>
2021-06-28 00:45:36 +0800

    1

---
+++ 01jvm/jvm01/src/main/java/com/javacourse/jvm01/Jvm01Application.java
@@ -0,0 +1,13 @@
+package com.javacourse.jvm01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jvm01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jvm01Application.class, args);
+    }
+
+}
---
+++ 01jvm/jvm01/src/test/java/com/javacourse/jvm01/Jvm01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacourse.jvm01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jvm01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
